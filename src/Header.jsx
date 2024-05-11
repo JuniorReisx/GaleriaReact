12b4c604876abd9affd7c82967c9f94d2bbdd107
Junior Reis <gilmaikjunior@gmail.com>
2024-05-11 10:50:00 -0300

    three commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
 
-import './Header.module.css'
+import style from './Header.module.css'
 
 export function Header() {
 
 
   return (
-   <header>
+   <header className={style.header}>
     <nav>
       <h1>Galeria</h1>
     </nav>
